5582480f94320e28f8192086ae3b8e4570eba08f
Alina <tyalinav@yandex.ru>
2023-09-09 01:44:20 +0300

    Create build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 javascript
-   module.exports = {
-     publicPath: process.env.NODE_ENV === 'production' ? '/yandex_practicum/' : '/'
-   }
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/yandex_practicum/' : '/',
+  
+    chainWebpack: (config) => {
+       config.module
+         .rule('mp4')
+         .test(/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
+         .use('file-loader')
+         .loader('file-loader')
+         .options({
+           name: 'media/[name].[hash:8].[ext]'
+         })
+         .end();
+     },
+  }
